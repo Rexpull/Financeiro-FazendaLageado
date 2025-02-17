f833a7099378aba0552c52d2248348d657fed27b
Rexpull <gamabrinbr@gmail.com>
2025-02-17 04:53:05 -0300

    correçao typo

--- frontend/src/components/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import loadingArt from "../assets/img/loadingArt.svg";
 
 const LoadingScreen: React.FC = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50" style={{paddingBottom: "10em"}}>
       <div className="flex items-center justify-center flex-col">
-        <img src="/frontend/src/assets/img/loadingArt.svg" alt="Loading"  className="object-contain" style={{width:"22em", height:"22em"}} />
+        <img src={loadingArt} alt="Loading"  className="object-contain" style={{width:"22em", height:"22em"}} />
         <p className="loading-text">O Painel Financeiro está sendo carregado.
         </p>
       </div>
