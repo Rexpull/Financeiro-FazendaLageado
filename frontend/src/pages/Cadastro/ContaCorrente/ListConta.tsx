27099fa8ff1e7004ab9b0b56d02262261a7fdd3e
Rexpull <gamabrinbr@gmail.com>
2025-02-17 04:48:02 -0300

    ajustado caminho fotos

--- frontend/src/pages/Cadastro/ContaCorrente/ListConta.tsx
+++ frontend/src/pages/Cadastro/ContaCorrente/ListConta.tsx
@@ -8,25 +8,25 @@ import { ContaCorrente } from "../../../../../backend/src/models/ContaCorrente";
 
 // Importando as imagens dos bancos
 import bancoBrasil from "../../../assets/img/icon-Bancos/banco-brasil.svg";
-import santander from "../../assets/img/icon-Bancos/santander.svg";
-import caixa from "../../assets/img/icon-Bancos/caixa.svg";
-import bradesco from "../../assets/img/icon-Bancos/bradesco.svg";
-import itau from "../../assets/img/icon-Bancos/itau.svg";
-import inter from "../../assets/img/icon-Bancos/inter.svg";
-import sicredi from "../../assets/img/icon-Bancos/sicredi.svg";
-import sicoob from "../../assets/img/icon-Bancos/sicoob.svg";
-import safra from "../../assets/img/icon-Bancos/safra.svg";
-import nubank from "../../assets/img/icon-Bancos/nubank.svg";
-import original from "../../assets/img/icon-Bancos/original.svg";
-import bancoBrasilia from "../../assets/img/icon-Bancos/banco-brasilia.svg";
-import banrisul from "../../assets/img/icon-Bancos/banrisul.svg";
-import citiBank from "../../assets/img/icon-Bancos/citi-bank.svg";
-import hsbc from "../../assets/img/icon-Bancos/hsbc.svg";
-import banestes from "../../assets/img/icon-Bancos/banestes.svg";
-import bancoAmazonia from "../../assets/img/icon-Bancos/banco-amazonia.svg";
-import bancoNordeste from "../../assets/img/icon-Bancos/banco-nordeste.svg";
-import bankBoston from "../../assets/img/icon-Bancos/bank-boston.svg";
-import defaultIcon from "../../assets/img/icon-Bancos/default.svg";
+import santander from "../../../assets/img/icon-Bancos/santander.svg";
+import caixa from "../../../assets/img/icon-Bancos/caixa.svg";
+import bradesco from "../../../assets/img/icon-Bancos/bradesco.svg";
+import itau from "../../../assets/img/icon-Bancos/itau.svg";
+import inter from "../../../assets/img/icon-Bancos/inter.svg";
+import sicredi from "../../../assets/img/icon-Bancos/sicredi.svg";
+import sicoob from "../../../assets/img/icon-Bancos/sicoob.svg";
+import safra from "../../../assets/img/icon-Bancos/safra.svg";
+import nubank from "../../../assets/img/icon-Bancos/nubank.svg";
+import original from "../../../assets/img/icon-Bancos/original.svg";
+import bancoBrasilia from "../../../assets/img/icon-Bancos/banco-brasilia.svg";
+import banrisul from "../../../assets/img/icon-Bancos/banrisul.svg";
+import citiBank from "../../../assets/img/icon-Bancos/citi-bank.svg";
+import hsbc from "../../../assets/img/icon-Bancos/hsbc.svg";
+import banestes from "../../../assets/img/icon-Bancos/banestes.svg";
+import bancoAmazonia from "../../../assets/img/icon-Bancos/banco-amazonia.svg";
+import bancoNordeste from "../../../assets/img/icon-Bancos/banco-nordeste.svg";
+import bankBoston from "../../../assets/img/icon-Bancos/bank-boston.svg";
+import defaultIcon from "../../../assets/img/icon-Bancos/default.png";
 
 
 const ListConta: React.FC = () => {
